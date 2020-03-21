8302020a2141b18497cacdabbeae83477aeee9ec
karthikpothineni <41578141+karthikpothineni@users.noreply.github.com>
2020-03-21 11:03:15 +0530

    Add files via upload

---
+++ contact_me.php
@@ -0,0 +1,48 @@
+<?php
+require 'vendor/autoload.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+// Check for empty fields
+if(empty($_POST['name'])        ||
+   empty($_POST['email'])       ||
+   empty($_POST['phone'])       ||
+   empty($_POST['message']) ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+    print "No arguments Provided!";
+    return false;
+   }
+
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+
+// Create the email and send the message
+// $to = 'pvskarthik94@gmail.com,pprahladarao@gmail.com,karthikpvs94@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+// $email_subject = "Website Contact Form:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";
+// mail($to,$email_subject,$email_body,$headers);
+
+# Instantiate the client.
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("postman@pvsconstructions.com", "Customer");
+$email->setSubject("Request for House");
+$email->addTo("karthikpvs94@gmail.com","pvskarthik94@gmail.com","pvindubhanu96@gmail.com","pvindubhanu@gmail.com","pprahladarao@gmail.com");
+$email->addContent("text/plain", "We got message from {$name} and details are Email: {$email_address}, Phone: {$phone}, Message: {$message}");
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    echo $response->statusCode() . "\n";
+    echo ($response->headers());
+    echo $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
+return true;
+?>
